2a1f7c5aa0b087363824b99948956d94fb7d8bf8
Simon Dahal <simon.dahal@coderacademy.edu.au>
2024-11-06 20:03:41 +1100

    status codes and post method

--- app.py
+++ app.py
@@ -31,6 +31,28 @@ products = [
 def get_products():
     return products
 
+@app.route("/products/0")
+def product_0():
+    product0 = products[0]
+    return product0
+
+@app.route("/products/1")
+def product_1():
+    product1 = products[1]
+    return product1
+
+@app.route("/products/2")
+def product_2():
+    return {"message": "Product 2 does not exist"}, 404
+
+@app.route("/products", methods=["POST"])
+def create_product():
+    # get product information from body of the request
+    body_data = request.get_json()
+    # print(body_data)
+    products.append(body_data)
+    return products
+
 @app.route("/can_vote")
 def can_vote():
     age = int(request.args.get('age'))
